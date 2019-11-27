f43ded66377352734a4bbc6caa9f5304b91600d7
Nikita Chistyakov <ofigate@gmail.com>
2019-11-27 16:36:15 +0300

    docs++

--- figures.py
+++ figures.py
@@ -1,86 +1,53 @@
-import glob
-import io
-import os
+from glob import glob
 from pprint import pprint
 
 from structs import NeuralNetwork
+from tools import read_img_file, get_output
 
 
-def makeInputArray(imgContent):
-    result = []
-    values = {}
-
-    j = 0
-    findMarks = 2
-
-    while findMarks:
-        if not (str(imgContent[j]).find("#000000") == -1):
-            values[str(imgContent[j])[3]] = 1
-            findMarks -= 1
-        if not (str(imgContent[j]).find("#FFFFFF") == -1):
-            values[str(imgContent[j])[3]] = 0
-            findMarks -= 1
-        j += 1
-
-    for i in range(j, len(imgContent)):
-        for j in range(3, len(imgContent[i])):
-            if not (str(imgContent[i])[j] == '"'):
-                result.append(values[str(imgContent[i])[j]])
-            else:
-                break
-
-    return result
-
-
-def readImgFile(img):
-    imgContent = io.BytesIO()
-    fileLink = io.open(img, 'r+b', 0)
-    imgContent = fileLink.readlines()
-    arr = makeInputArray(imgContent)
-
-    return arr
-
-
-def getOutput(i):
-    result = []
-
-    for j in range(2):
-        result.append(0)
-
-    result[i] = 1
-
-    return result
-
-
+# начинаем составлять обучающую выборку
 to_learn = []
 
+# для каждой директории с обучающими выборками (соответствующими распознаваемой цифре)
 for digit in range(2):
-    files = glob.glob("figures/{}/*.xpm".format(str(digit)))
+    # получаем список путей к файлам изображений в ней
+    files = glob("figures/{}/*.xpm".format(str(digit)))
 
+    # для каждого пути к файлу
     for img in files:
-        arr = readImgFile(img)
-        to_learn.append([arr, getOutput(digit)])
+        # читаем все пиксели файла в массив
+        arr = read_img_file(img)
 
-# print(to_learn)
+        # добавляем набор входов (пикселей) и эталон в обучающую выборку
+        to_learn.append([arr, get_output(digit)])
 
-nn = NeuralNetwork()
-nn.add_input_layer(256)
-nn.add_hidden_layer(64)
-nn.add_hidden_layer(64)
-nn.add_hidden_layer(64)
-nn.add_hidden_layer(16)
-nn.add_output_layer(2)
+nn = NeuralNetwork()  # создаем нейронную сеть
+nn.add_input_layer(256)  # добавляем входной слой
+nn.add_hidden_layer(64)  # добавляем скрытый слой
+nn.add_hidden_layer(64)  # добавляем скрытый слой
+nn.add_hidden_layer(64)  # добавляем скрытый слой
+nn.add_hidden_layer(16)  # добавляем скрытый слой
+nn.add_output_layer(2)  # добавляем выходной слой
 
+# в цикле совершаем заданное количество эпох обучения
 for i in range(0, 100):
     print('')
     print('EPOCH #{}'.format(i))
     loss_total = nn.train(to_learn, 1)
     print('{:.4f}'.format(loss_total))
 
-files = glob.glob("figures/test/*.xpm")
+# затем получаем пути к файлам из тестовой выборки
+files = glob("figures/test/*.xpm")
+
+# для каждого пути к файлу
 for img in files:
-    arr = readImgFile(img)
+    # читаем все пиксели файла в массив
+    arr = read_img_file(img)
+
+    # подаем данные на сеть и считаем результат
     nn.run(arr)
+
+    # выводим результат
     print()
     print(img)
     pprint(repr(nn.get_output_layer()))
---
+++ tools.py
@@ -0,0 +1,70 @@
+import io
+
+
+def make_input_array(img_content):
+    """Из данных изображения составляет список значений для входного слоя нейронной сети
+
+    :param img_content: массив содержимого файла изображения
+    :type img_content: list
+    :return: список значений для входного слоя нейронной сети
+    :rtype: list[float]
+
+    """
+    result = []
+    values = {}
+
+    j = 0
+    find_marks = 2
+
+    while find_marks:
+        if not (str(img_content[j]).find("#000000") == -1):
+            values[str(img_content[j])[3]] = 1
+            find_marks -= 1
+        if not (str(img_content[j]).find("#FFFFFF") == -1):
+            values[str(img_content[j])[3]] = 0
+            find_marks -= 1
+        j += 1
+
+    for i in range(j, len(img_content)):
+        for j in range(3, len(img_content[i])):
+            if not (str(img_content[i])[j] == '"'):
+                result.append(values[str(img_content[i])[j]])
+            else:
+                break
+
+    return result
+
+
+def read_img_file(img_path):
+    """Читает файл и получает из него список значений для входного слоя нейронной сети
+
+    :param img_path: путь к файлу изображения
+    :type img_path: str
+    :return: список значений для входного слоя нейронной сети
+    :rtype: list[float]
+
+    """
+    file_link = io.open(img_path, 'r+b', 0)
+    img_content = file_link.readlines()
+    arr = make_input_array(img_content)
+
+    return arr
+
+
+def get_output(i):
+    """Составляет эталонный набор значений, в котором все нули, а i-й элемент — 1
+
+    :param i: индекс выхода, который будет установлен в 1
+    :type i: int
+    :return: эталонный набор значений
+    :rtype: list[float]
+
+    """
+    result = []
+
+    for j in range(10):
+        result.append(0)
+
+    result[i] = 1
+
+    return result
