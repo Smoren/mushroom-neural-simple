83d2ca506f1078d1642f48f1ee6f0bfd40345820
Smoren <ofigate@gmail.com>
2021-03-28 18:56:49 +0300

    fingers++

---
+++ fingers.py
@@ -0,0 +1,64 @@
+import json
+
+from structs import NeuralNetwork
+import tools
+
+DATA_FILE_NAME = 'output/fingers.json'
+MOTION_COUNT = 3
+EXAMPLES_COUNT = 5
+FRAMES_COUNT = 20
+FRAME_SIZE = 5
+TRAIN_EPOCH_COUNT = 1000
+
+nn = NeuralNetwork()  # создаем нейронную сеть
+
+import_data = tools.import_json_file(DATA_FILE_NAME)  # получим сохраненные данные связей, если сеть ранее обучали
+
+if import_data:
+    nn.import_(import_data)  # выполним импорт связей в сеть
+else:
+    dirty_data = []
+
+    for motion in range(MOTION_COUNT):
+        for example in range(EXAMPLES_COUNT):
+            dirty_data.append(tools.import_json_file('input/fingers/{}/{}.json'.format(motion, example)))
+
+    input_layer_size = FRAMES_COUNT * FRAME_SIZE
+    output_layer_size = MOTION_COUNT
+
+    nn.add_input_layer(input_layer_size)  # добавляем входной слой
+    # nn.add_hidden_layer(5)  # добавляем скрытый слой
+    nn.add_output_layer(output_layer_size)  # добавляем выходной слой
+
+    # готовим обучающую выборку
+    train_data = []
+
+    for example in dirty_data:
+        input_data = []
+        output_data = [0 for i in range(MOTION_COUNT)]
+        output_data[example['output']] = 1
+        for frame in example['input']:
+            for finger_value in frame:
+                input_data.append(finger_value)
+
+        train_data.append([input_data, output_data])
+
+    epoch_losses = []
+
+    # выполняем заданное количество эпох обучения
+    for i in range(0, TRAIN_EPOCH_COUNT):
+        print('')
+        print('EPOCH #{}'.format(i))
+        loss_total = nn.train(train_data, 0.1)
+        epoch_losses.append(loss_total)
+        print('{:.4f}'.format(loss_total))
+
+    tools.export_json_file(DATA_FILE_NAME, nn.export())
+
+    print('EPOCH LOSSES PROGRESS: ')
+    print(epoch_losses)
+
+# nn.run([1, 0, 0])  # выставляем на входы сигналы и выполняем прямой проход
+
+# print()
+# print(nn.get_output())
