fce0a827f1f01131f88184cafc7846b00455483f
Smoren <ofigate@gmail.com>
2022-10-22 16:57:26 +0300

    upd

--- test_images.py
+++ test_images.py
@@ -1,3 +1,5 @@
+from random import shuffle
+
 import numpy as np
 
 import activation
@@ -8,40 +10,47 @@ from structs import NeuralNetwork
 DATA_FILE_NAME = 'output/images.json'
 
 nn = NeuralNetwork()  # создаем нейронную сеть
-nn.add_input_layer(2)  # добавляем входной слой
-nn.add_layer(4, activation_class=activation.ActivationSigmoid, random_radius=2)  # добавляем скрытый слой
-nn.add_layer(4, activation_class=activation.ActivationSigmoid, random_radius=2)  # добавляем скрытый слой
-nn.add_layer(3, activation_class=activation.ActivationSigmoid, random_radius=2)  # добавляем выходной слой
+nn.add_input_layer(3)  # добавляем входной слой
+nn.add_layer(64, activation_class=activation.ActivationRelu, random_radius=0.5, use_bias=True)
+nn.add_layer(64, activation_class=activation.ActivationSigmoid, random_radius=0.5, use_bias=True)
+nn.add_layer(3, activation_class=activation.ActivationSigmoid, random_radius=0.5, use_bias=True)
 
 import_data = tools.import_json_file(DATA_FILE_NAME)  # получим сохраненные данные связей, если сеть ранее обучали
 input_img = open_img('input/images/1.jpg')
+train_data = []
 
-if import_data:
+if False and import_data:
     nn.import_(import_data)  # выполним импорт связей в сеть
 else:
     # готовим обучающую выборку
-    train_data = []
-
     for y in range(input_img.shape[0]):
         for x in range(input_img.shape[1]):
             pixel = input_img[y][x]
             train_data.append([
-                [y/input_img.shape[0], x/input_img.shape[1]],
+                [y/input_img.shape[0], x/input_img.shape[1], 1],
                 [pixel[0]/256, pixel[1]/256, pixel[2]/256]
             ])
 
     # выполняем заданное количество эпох обучения
-    for i in range(0, 200):
+    for i in range(0, 100):
+        shuffle(train_data)
         print('')
         print('EPOCH #{}'.format(i))
-        loss_total = nn.train(train_data, 0.1, False)
+        loss_total = nn.train(train_data, 0.7, False)
         print('LOSS: {:.4f}'.format(loss_total))
 
-    # tools.export_json_file(DATA_FILE_NAME, nn.export())
+    tools.export_json_file(DATA_FILE_NAME, nn.export())
+
+# for (y, x), pixel in train_data:
+#     y = int(y*input_img.shape[0])
+#     x = int(x*input_img.shape[1])
+#     input_img[y][x][0] = pixel[0]*256
+#     input_img[y][x][1] = pixel[1]*256
+#     input_img[y][x][2] = pixel[2]*256
 
 for y in range(input_img.shape[0]):
     for x in range(input_img.shape[1]):
-        pixel = nn.run([y/input_img.shape[0], x/input_img.shape[1]]).get_output()
+        pixel = nn.run([y/input_img.shape[0], x/input_img.shape[1], 1]).get_output()
         input_img[y][x][0] = pixel[0]*256
         input_img[y][x][1] = pixel[1]*256
         input_img[y][x][2] = pixel[2]*256
